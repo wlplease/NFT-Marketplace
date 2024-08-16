55e7ce1172a12e44b4669ffb5330b5962b4deaf1
mintpic dev <108204467+mintpic@users.noreply.github.com>
2024-08-16 09:57:46 -0400

    New updates voloview.com

--- config-overrides.js
+++ config-overrides.js
@@ -1,19 +1,21 @@
 const webpack = require('webpack');
 
 module.exports = function override(config) {
-  // Add fallback for process, stream, and buffer modules
+  // Add fallback for process and other node modules
   config.resolve.fallback = {
     process: require.resolve('process/browser'),
     stream: require.resolve('stream-browserify'),
     buffer: require.resolve('buffer/'),
-    path: require.resolve('path-browserify')
+    path: require.resolve('path-browserify'),
+    zlib: require.resolve('browserify-zlib'),
+    crypto: require.resolve('crypto-browserify'),
   };
 
   // Add ProvidePlugin for process and Buffer
   config.plugins.push(
     new webpack.ProvidePlugin({
       process: 'process/browser',
-      Buffer: ['buffer', 'Buffer']
+      Buffer: ['buffer', 'Buffer'],
     })
   );
 
@@ -21,7 +23,7 @@ module.exports = function override(config) {
   config.module.rules.push({
     test: /\.mjs$/,
     include: /node_modules/,
-    type: "javascript/auto"
+    type: 'javascript/auto',
   });
 
   return config;
