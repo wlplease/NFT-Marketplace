daf28b6ed2fd91e1e016a03444f73dfea455cbf0
mintpic dev <108204467+mintpic@users.noreply.github.com>
2024-08-16 10:10:38 -0400

    New updates voloview.com

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,17 +1,19 @@
 const webpack = require('webpack');
 
-module.exports = function override(config, env) {
+module.exports = function override(config) {
+  // Add fallback for process and buffer modules
   config.resolve.fallback = {
     process: require.resolve('process/browser'),
     stream: require.resolve('stream-browserify'),
-    buffer: require.resolve('buffer/'),
+    buffer: require.resolve('buffer/')
   };
 
+  // Add ProvidePlugin for process and Buffer
   config.plugins.push(
     new webpack.ProvidePlugin({
       process: 'process/browser',
-      Buffer: ['buffer', 'Buffer'],
-    }),
+      Buffer: ['buffer', 'Buffer']
+    })
   );
 
   return config;
